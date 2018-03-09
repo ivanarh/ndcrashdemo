a773d966fe57464a7d378e446667dbe534fe9439
Ivan Ponomarev <ivan.ponomarev@akvelon.com>
2018-03-09 16:46:02 +0300

    Added backend selection support from java code.

--- app/src/main/java/ru/ivanarh/ndcrashdemo/MainActivity.java
+++ app/src/main/java/ru/ivanarh/ndcrashdemo/MainActivity.java
@@ -1,9 +1,15 @@
 package ru.ivanarh.ndcrashdemo;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
@@ -48,8 +54,34 @@ public class MainActivity extends Activity {
             }
         });
 
+        final Spinner backendForNextLaunch = findViewById(R.id.backend_for_next_launch_spinner);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_spinner_item,
+                new String[] { "libcorkscrew", "libunwind", "libunwindstack" } // should match ndcrash_backend order.
+                );
+        backendForNextLaunch.setAdapter(adapter);
+        backendForNextLaunch.setSelection(getSharedPreferences(MainApplication.SHARED_PREFS_NAME, MODE_PRIVATE).
+                getInt(MainApplication.BACKEND_FOR_NEXT_LAUNCH_KEY, 0));
+        backendForNextLaunch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                saveBackendToSettings(i);
+            }
+
+            @Override public void onNothingSelected(AdapterView<?> adapterView) {
+                saveBackendToSettings(0);
+            }
+        });
+
         mNativeCrashTextField = (TextView) findViewById(R.id.lastCrashContents);
         mNativeCrashTextFieldDefaultValue = mNativeCrashTextField.getText().toString();
+    }
+
+    private void saveBackendToSettings(int backend) {
+        getSharedPreferences(MainApplication.SHARED_PREFS_NAME, MODE_PRIVATE)
+                .edit()
+                .putInt(MainApplication.BACKEND_FOR_NEXT_LAUNCH_KEY, backend)
+                .apply();
     }
 
     @Override
--- app/src/main/java/ru/ivanarh/ndcrashdemo/MainApplication.java
+++ app/src/main/java/ru/ivanarh/ndcrashdemo/MainApplication.java
@@ -1,18 +1,21 @@
 package ru.ivanarh.ndcrashdemo;
 
 import android.app.Application;
+import android.content.Context;
 
 import ru.ivanarh.jndcrash.NDCrash;
 
 
 public class MainApplication extends Application {
 
+    public static final String SHARED_PREFS_NAME = "jndcrash";
+    public static final String BACKEND_FOR_NEXT_LAUNCH_KEY = "backend_for_next_launch";
     public static String mNativeCrashPath;
 
     @Override
     public void onCreate() {
         super.onCreate();
         mNativeCrashPath = getFilesDir().getAbsolutePath() + "/crash.txt";
-        NDCrash.initialize(mNativeCrashPath);
+        NDCrash.initialize(mNativeCrashPath, getSharedPreferences(SHARED_PREFS_NAME, MODE_PRIVATE).getInt(BACKEND_FOR_NEXT_LAUNCH_KEY, 0));
     }
 }
